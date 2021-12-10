a226bbb09c4b6b9ed8842ddf8ad867f5372137a9
podesport <88339411+podesport@users.noreply.github.com>
2021-12-10 21:04:38 +0700

    create all class

--- src/entity/Maincharacter.java
+++ src/entity/Maincharacter.java
@@ -1,5 +1,92 @@
 package entity;
 
 public class Maincharacter {
+	
+	protected String name;
+    protected String description;
+    protected int health;
+    protected int maxHealth;
+    protected int mana;
+    protected int maxMana;
+    protected int attack;
+    protected int defense ;
+    protected PlayType type ;
+    
+    public Maincharacter() {
+    	
+    }
 
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public int getHealth() {
+		return health;
+	}
+
+	public void setHealth(int health) {
+		this.health = health;
+	}
+
+	public int getMaxHealth() {
+		return maxHealth;
+	}
+
+	public void setMaxHealth(int maxHealth) {
+		this.maxHealth = maxHealth;
+	}
+
+	public int getMana() {
+		return mana;
+	}
+
+	public void setMana(int mana) {
+		this.mana = mana;
+	}
+
+	public int getMaxMana() {
+		return maxMana;
+	}
+
+	public void setMaxMana(int maxMana) {
+		this.maxMana = maxMana;
+	}
+
+	public int getAttack() {
+		return attack;
+	}
+
+	public void setAttack(int attack) {
+		this.attack = attack;
+	}
+
+	public int getDefense() {
+		return defense;
+	}
+
+	public void setDefense(int defense) {
+		this.defense = defense;
+	}
+
+	public PlayType getType() {
+		return type;
+	}
+
+	public void setType(PlayType type) {
+		this.type = type;
+	}
+    
+    
 }
