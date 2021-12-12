b2096bccd457998aed09117d3f0162cc35808a3a
podesport <88339411+podesport@users.noreply.github.com>
2021-12-12 22:44:12 +0700

    555

--- src/logic/GamePlay.java
+++ src/logic/GamePlay.java
@@ -2,13 +2,16 @@ package logic;
 
 import java.util.ArrayList;
 
+import Potion.Potion;
 import entity.Player;
 
 public class GamePlay {
 	
 	protected static ArrayList<Player> myChar ;
 	protected static int money ;
-	protected static ArrayList<E> myPotion ;
+	protected static ArrayList<Potion> myPotion ;
+	
+	
 	
 
 	
